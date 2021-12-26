1e98c6510af3ac80a1555a51ac30444829d11904
Alexandru <alexandruolteanu2001@gmail.com>
2021-12-26 09:51:43 -0800

    Adding new constants

---
+++ src/common/Constants.java
@@ -0,0 +1,24 @@
+package common;
+
+public final class Constants {
+    public static final String OUTPUT_PATH = "output/out_";
+    public static final String REF_PATH = "ref/ref_test";
+    public static final String FILE_EXTENSION = ".json";
+    public static final Integer SMALL_TEST_POINTS = 2;
+    public static final Integer BIG_TEST_POINTS = 3;
+    public static final Integer TESTS_NUMBER = 25;
+    public static final Integer MAXIMUM_ERROR_CHECKSTYLE = 30;
+    public static final Integer CHECKSTYLE_POINTS = 10;
+    public static final Integer TESTS_NUMBER_SMALL = 15;
+    public static final Integer BABY_AGE = 5;
+    public static final Integer KID_AGE = 12;
+    public static final Integer TEEN_AGE = 18;
+    public static final Double BABY_AVERAGE_SCORE = 10.0;
+
+
+
+    private Constants() {
+        //constructor for checkstyle
+    }
+
+}
